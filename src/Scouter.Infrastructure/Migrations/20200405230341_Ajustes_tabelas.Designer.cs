e22c33472c26179caef48a84f20a2d06c1207e13
Jorge Braz <jorge.braz@intelectah.com.br>
2020-04-05 20:05:07 -0300

    Ajuste tabela base

--- src/Scouter.Infrastructure/Migrations/20200405230341_Ajustes_tabelas.Designer.cs
+++ src/Scouter.Infrastructure/Migrations/20200405230341_Ajustes_tabelas.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Scouter.Infrastructure.Context;
 
 namespace Scouter.Infrastructure.Migrations
 {
     [DbContext(typeof(ScouterContext))]
-    partial class ScouterContextModelSnapshot : ModelSnapshot
+    [Migration("20200405230341_Ajustes_tabelas")]
+    partial class Ajustes_tabelas
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Scouter.Infrastructure/Migrations/20200405230341_Ajustes_tabelas.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Scouter.Infrastructure.Migrations
+{
+    public partial class Ajustes_tabelas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Usuario",
+                newName: "UsuarioId");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataCriacao",
+                table: "Position",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UsuarioId",
+                table: "Position",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DataCriacao",
+                table: "Level",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UsuarioId",
+                table: "Level",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DataCriacao",
+                table: "Position");
+
+            migrationBuilder.DropColumn(
+                name: "UsuarioId",
+                table: "Position");
+
+            migrationBuilder.DropColumn(
+                name: "DataCriacao",
+                table: "Level");
+
+            migrationBuilder.DropColumn(
+                name: "UsuarioId",
+                table: "Level");
+
+            migrationBuilder.RenameColumn(
+                name: "UsuarioId",
+                table: "Usuario",
+                newName: "Id");
+        }
+    }
+}
